5fbe4d9c40ca60cdf7921ed5b514a734e5474155
sawyer tarr <sawyertarr@outlook.com>
2023-02-08 15:25:29 -0600

    ThreeMethods

---
+++ MethodOverloadingExercise/Method.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExercise
+{
+    internal class Method
+    {
+        public static int Add(int num1, int num2)
+        {
+            
+           return num1 + num2;
+
+        } 
+        public static decimal Add(decimal num1, decimal num2) 
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool yes)
+        {
+            int sum = num1+ num2;
+
+            if (yes == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (yes == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (yes == true && sum < 1) 
+            {  
+                return $"{sum}  dollars";
+            }
+            else
+            {
+                 return sum.ToString();
+            }
+        }
+    }
+}
---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using MethodOverloadingExercise;
+
+
+//First Method
+Console.WriteLine(Method.Add(1,2));
+
+//Second Method
+
+decimal num1 = 1.1m;
+decimal num2 = 2.1m;
+
+Console.WriteLine(Method.Add(num1, num2));
+
+//Third Method
+
+Console.WriteLine(Method.Add(2, 3, true));
